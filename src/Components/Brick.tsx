55c631c2caf9029bfb25319e750be751b1a9db68
Mathieu Fedrigo <mathieuf@bam.tech>
2022-10-28 18:16:06 +0200

    add sides left right

--- src/Components/Brick.tsx
+++ src/Components/Brick.tsx
@@ -6,19 +6,27 @@ import { ISide } from '../Types/Side';
 
 export const Brick: FC<{number: IBrick, side: ISide}> = ({number, side}) => {
   return (
-    <View style={[styles.container, {backgroundColor: side === 'left' ? 'cyan' : 'pink'}]}>
-      <Text>{number}</Text>
+    <View style={[styles.container, {justifyContent: side === 'left' ? 'flex-end' : 'flex-start'}]}>
+      <View style={[styles.brick, {backgroundColor: side === 'left' ? 'cyan' : 'pink'}]}>
+        <Text>{number}</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    width: '80%',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 8,
+  },
+  brick: {
     width: 200,
     height: 150,
     backgroundColor: 'pink',
     alignItems: 'center',
     justifyContent: 'center',
-    marginBottom: 8
   },
 });
