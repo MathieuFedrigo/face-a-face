2e34369f726a4a846d75320f08f49de62c3d4ecd
Mathieu Fedrigo <mathieuf@bam.tech>
2022-10-29 13:01:06 +0200

    add filling

--- src/Components/Brick.tsx
+++ src/Components/Brick.tsx
@@ -1,11 +1,21 @@
 import { FC } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { timeSelector } from '../redux/boardSlice';
+import { useAppSelector } from '../redux/hooks';
 import { IBrick } from '../Types/Brick';
 import { ISide } from '../Types/Side';
 
 export const Brick: FC<{number: IBrick, side: ISide}> = ({number, side}) => {
+  const timeLeft = useAppSelector(timeSelector);
+  const notStarted = timeLeft > 5000*(number) 
+  const finished = timeLeft < 5000*(number - 1) 
+  let fillingPercentage = number - timeLeft/5000
+  if (notStarted) fillingPercentage = 0
+  if (finished) fillingPercentage = 1
+
   return (
     <View style={[styles.container, {justifyContent: side === 'left' ? 'flex-end' : 'flex-start'}]}>
+      <View style={[styles.indicator, {height: fillingPercentage * 150}]}/>
       <View style={[styles.brick, {backgroundColor: side === 'left' ? 'cyan' : 'pink'}]}>
         <Text>{number}</Text>
       </View>
@@ -25,6 +35,15 @@ const styles = StyleSheet.create({
     width: 200,
     height: 150,
     backgroundColor: 'pink',
+    alignItems: 'center',
+    justifyContent: 'center',
+    opacity: 0.5,
+  },
+  indicator: {
+    position: 'absolute',
+    top: 0,
+    width: 200,
+    backgroundColor: 'grey',
     alignItems: 'center',
     justifyContent: 'center',
   },
